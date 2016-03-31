1d70f23643d26be2305039215cbe030bf635ae60
Martinho Aragão <martinhoaragao@gmail.com>
2016-03-31 01:20:34 +0100

    Improve log functions

--- authentication.hs
+++ authentication.hs
@@ -1,12 +1,12 @@
 import qualified Data.Map as DM
 import Data.Maybe
+import Control.Monad
 import Control.Concurrent.STM
 
-data User = User { username    :: String
-                 , password    :: String
+data User = User { getUsername :: String
+                 , getPassword :: String
                  , isConnected :: Bool
                  } deriving (Eq, Ord, Show)
-
 
 newtype Users = Users (TVar (DM.Map String User))
 
@@ -14,15 +14,31 @@ newtype Users = Users (TVar (DM.Map String User))
 registerSTM :: User -> Users -> STM ()
 registerSTM user (Users usersSTM) = do
   users <- readTVar usersSTM
-  writeTVar usersSTM $ DM.insert (username user) user users
+  writeTVar usersSTM $ DM.insert (getUsername user) user users
+
+register user users = atomically $ registerSTM user users
 
 isLoggedIn username (Users usersSTM) = do
   users <- readTVar usersSTM
   let user = DM.lookup username users
-  let res = not (isNothing user) && (isConnected . fromJust $ user)
+  let res = isJust user && (isConnected . fromJust $ user)
   return res
 
-newUsersSTM :: IO (Users)
+switchUserLogStatus username (Users usersSTM) = do
+  users <- readTVar usersSTM
+  let user = fromJust $ DM.lookup username users
+  let loggedUser =  User username (getPassword user) (not (isConnected user))
+  writeTVar usersSTM $ DM.insert username loggedUser users
+
+logInUser username users = atomically $ do
+  userLogStatus <- isLoggedIn username users
+  unless userLogStatus (switchUserLogStatus username users)
+
+logOutUser username users = atomically $ do
+  userLogStatus <- isLoggedIn username users
+  when userLogStatus (switchUserLogStatus username users)
+
+newUsersSTM :: IO Users
 newUsersSTM = do
   t <- newTVarIO DM.empty
   return (Users t)
@@ -30,8 +46,11 @@ newUsersSTM = do
 main :: IO ()
 main = do
   users <- newUsersSTM
-  atomically $ registerSTM (User "martinho" "soufixe" False) users
-  atomically $ registerSTM (User "rego" "naosoufixe" False) users
+  register (User "martinho" "soufixe" False) users
+  register (User "rego" "naosoufixe" False) users
   res <- atomically $ isLoggedIn "martinho" users
-  putStrLn $ show res
+  print res
+  logInUser "martinho" users
+  res2 <- atomically $ isLoggedIn "martinho" users
+  print res2
   putStrLn "Hello World"
