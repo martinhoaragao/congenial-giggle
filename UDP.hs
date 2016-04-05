e49796c78931db5a856f01e53e5d337bb327ce66
zenunomacedo <a72424@alunos.uminho.pt>
2016-04-06 00:33:26 +0100

    Add very basic UDP utilities

---
+++ UDP.hs
@@ -0,0 +1,148 @@
+module Main where
+import qualified Data.ByteString as BSL
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import GHC.IO.Handle
+import Control.Monad
+import Data.Binary
+import System.IO
+import Network.Socket
+import Network.Socket.ByteString as NSB
+import Network.BSD
+
+packetSize = headerSize + dataSize
+dataSize = 48
+headerSize = 0
+
+{-
+TODO:
+>FIX READALL
+Ler pacotes e analisar cabeçalho, para saber quando parar de ler (funcao readAll)
+
+-}
+
+
+getSockUDPClient :: HostName -> String -> IO Socket     
+getSockUDPClient hostname port =  withSocketsDo $ do
+ addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+ let serveraddr = Prelude.head addrinfos
+ sock <- socket (addrFamily serveraddr) Datagram defaultProtocol 
+ connect sock (addrAddress serveraddr)
+ return sock
+
+getSockUDPServer :: IO Socket     
+getSockUDPServer = withSocketsDo $ do
+ addrinfos <- getAddrInfo (Just (defaultHints {addrFlags=[AI_PASSIVE]})) Nothing (Just "4242")
+ let serveraddr = Prelude.head addrinfos
+ sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+ setSocketOption sock ReuseAddr 1
+ bind sock (addrAddress serveraddr)
+ return sock
+
+
+
+testSend = withSocketsDo $ do
+    sock <- getSockUDPClient "localhost" "4242"
+    h <- socketToHandle sock ReadWriteMode 
+    --sendFile ("in.txt") h
+    putStrLn "Sending!"
+    --sendAudio ("000001.mp3") h
+    sendAudio ("in.txt") h
+    hClose h
+    System.IO.putStrLn "Sent bytes!"
+
+
+testReceive = withSocketsDo $ do
+    sock <- getSockUDPServer
+    h <- socketToHandle sock ReadWriteMode
+    input <- getAudio ("out.mp3") h
+    --getFile ("out.txt") h 
+    --input <- BSL.hGet h 83
+    BSL.writeFile "out.txt" input
+    System.IO.putStrLn "Received and saved file!"
+
+main = do
+    k <- getChar
+    if k == '1' then testReceive
+        else testSend
+-- //////////////////////////////////////////////////// actual code below
+
+
+sendAudio :: FilePath -> Handle -> IO()
+sendAudio file handle = do
+    f <- BSL.readFile file
+    let l = BSL.length f
+    let nPackets = ceiling((fromIntegral l) / (fromIntegral dataSize))
+    let n = 8 --trabalhamos com int64 por enquant
+    BSL.hPut handle $ toStrict (encode (l::Int))
+    putStrLn("Sent nPackets: " ++ (show nPackets) ++ "\nSent bytes: " ++ (show l) );
+    hFlush handle
+    --BSL.hPut handle f
+    let list = separate f
+    mapM_ (sendDatagram handle) list
+    putStrLn("Sent data");
+    --hFlush handle
+
+getAudio :: FilePath -> Handle -> IO BSL.ByteString
+getAudio file handle = do
+    n <- BSL.hGet handle 8
+    let size = (decode $ fromStrict n) ::Int
+    let nPackets = ceiling((fromIntegral size) / (fromIntegral dataSize))
+    putStrLn ("Getting " ++ show(size) ++ " bytes!\nGetting " ++ show(nPackets) ++ " packets!")
+    --audio_file <- BSL.hGet handle size
+    almostAllPackets <- getPackets handle (nPackets-1)
+    putStrLn ("Got it almost all!")
+    lastPacket <- BSL.hGet handle (size - ((nPackets-1) * packetSize))
+    putStrLn ("Got Last packet...")
+    let return_val = (BSL.concat (almostAllPackets ++ [lastPacket]))
+    putStrLn ("Data size: " ++ (show (BSL.length return_val)))
+    return return_val
+
+getPackets :: Handle -> Int -> IO [BSL.ByteString]
+getPackets handle 0 = return []
+getPackets handle n = do
+    t <- getPackets handle (n-1)
+    putStrLn ("Got packet no. " ++ (show n))
+    h <- BSL.hGet handle packetSize
+    return (t++[h])
+
+
+
+sendFile :: FilePath -> Handle -> IO ()
+sendFile file handle = do
+    f <- BSL.readFile file
+    let list = separate f
+    --mapM_ (sendDatagram handle) list
+    sendDatagram handle f
+
+sendDatagram :: Handle -> BSL.ByteString -> IO ()
+sendDatagram handle packet = do
+    BSL.hPut handle packet
+    hFlush handle
+
+separate :: BSL.ByteString -> [BSL.ByteString]
+separate l = separate' (fromIntegral packetSize) l
+ where separate' n l = if BSL.null l then [] else (a):(separate' n b)
+        where (a,b) = BSL.splitAt n l
+
+getFile :: FilePath -> Handle -> IO ()
+getFile file handle = do
+    audio_file <- readAll handle
+    --audio_file <- BSL.hGetContents handle
+    let final_audio = reconstruct audio_file
+    BSL.writeFile file final_audio
+
+reconstruct = BSL.concat
+
+readAll :: Handle -> IO [BSL.ByteString]
+readAll h = do
+    System.IO.putStrLn "read one"
+    p <- getDatagram h
+    --case tipo p of 
+        --dados -> readAll h
+        --mensagem de fim de envios -> if (completo) then return else pedir retransmissao
+    t <- readAll h
+    return (p:t)
+
+getDatagram :: Handle -> IO BSL.ByteString
+getDatagram handle = do
+    BSL.hGet handle packetSize
